bc4e70b67cf0da9dbfdf4608efb0f15de482ad22
Nick Nauert <nicknauert@gmail.com>
2017-11-20 22:46:06 -0500

    alternating walk

--- app.js
+++ app.js
@@ -2,32 +2,19 @@ const j5 = require('johnny-five');
 const board = new j5.Board();
 
 board.on('ready', () => {
-    const servo = new j5.Servo({
-        pin: 10,
-        startAt: 80,
-        range: [0, 160]
-    })
-    const servo2 = new j5.Servo({
-        pin: 11,
-        startAt: 80,
-        range: [0, 160]
-    })
-    const leftWalk = new j5.Animation(servo);
-    const rightWalk = new j5.Animation(servo2);
+    const servos = new j5.Servos([10, 11])
+    const leftWalk = new j5.Animation(servos);
 
-    setTimeout(startWalkAnim, 2000);
+    setTimeout(startWalkAnim, 1000);
     
     function startWalkAnim(){
         leftWalk.enqueue({
-            cuePoints: [0, .5, 1],
-            keyFrames: [80, {value: 0, easing: "linear"}, {value: 160, easing: "linear"}],
-            duration: 3000,
-            loop: true
-        })
-        rightWalk.enqueue({
-            cuePoints: [0, .5, 1 ],
-            keyFrames: [80, {value: 160, easing: "linear"}, {value: 0, easing: "linear"}],
-            duration: 3000,
+            cuePoints: [0, .33, .66, 1],
+            keyFrames: [
+                [{degrees: 80}, {degrees: 0}, {degrees: 80}, {degrees: 160}],
+                [{degrees: 80}, {degrees: 160}, {degrees: 80}, {degrees: 0}]
+            ],
+            duration: 1500,
             loop: true
         })
     }
