145b4d50eea230425b1b6e49ed95b35e4a398c84
Nick Nauert <nicknauert@gmail.com>
2017-11-20 22:25:32 -0500

    refining

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const j5 = require('johnny-five');
 const board = new j5.Board();
 
-board.on('ready', ()=>{
-    const led = new j5.Led(13);
-    led.blink(1000);
+board.on('ready', () => {
     const servo = new j5.Servo({
         pin: 10,
-        center: true,
+        startAt: 80,
         range: [0, 160]
     })
     const servo2 = new j5.Servo({
         pin: 11,
-        center: true,
+        startAt: 80,
         range: [0, 160]
     })
+    const leftWalk = new j5.Animation(servo);
+    const rightWalk = new j5.Animation(servo2);
+
+    setTimeout(startWalkAnim, 2000);
     
-    // let direction = true;
+    function startWalkAnim(){
+        leftWalk.enqueue({
+            cuePoints: [0, .5, 1],
+            keyFrames: [80, {value: 0, easing: "linear"}, {value: 160, easing: "linear"}],
+            duration: 3000,
+            loop: true
+        })
+        rightWalk.enqueue({
+            cuePoints: [0, .5, 1 ],
+            keyFrames: [80, {value: 160, easing: "linear"}, {value: 0, easing: "linear"}],
+            duration: 3000,
+            loop: true
+        })
+    }
+
+
+
+
+
+
+
+     // let direction = true;
     // function sprinkler(){
     //     if( direction ){
     //         console.log('up');
@@ -31,23 +54,4 @@ board.on('ready', ()=>{
     // }
     // sprinkler();
     // servo.on('move:complete', sprinkler)
-
-    const leftWalk = new j5.Animation(servo);
-    const rightWalk = new j5.Animation(servo2);
-
-    leftWalk.enqueue({
-        cuePoints: [0, .33, 1],
-        keyFrames: [80, {value: 0, easing: "linear"}, {value: 160, easing: "linear"}],
-        duration: 3000,
-        loop: true
-    })
-    rightWalk.enqueue({
-        cuePoints: [0, .33, 1],
-        keyFrames: [80, {value: 160, easing: "linear"}, {value: 0, easing: "linear"}],
-        duration: 3000,
-        loop: true
-    })
-    
-    
-
 })
