4301435a1a2e035ad3527ad164fbbc015f8e6eff
Nick Nauert <nicknauert@gmail.com>
2017-11-20 22:15:37 -0500

    Walking motion with 2 servos

--- app.js
+++ app.js
@@ -7,23 +7,47 @@ board.on('ready', ()=>{
     const servo = new j5.Servo({
         pin: 10,
         center: true,
-        range: [30, 120]
+        range: [0, 160]
+    })
+    const servo2 = new j5.Servo({
+        pin: 11,
+        center: true,
+        range: [0, 160]
     })
     
-    let direction = true;
+    // let direction = true;
+    // function sprinkler(){
+    //     if( direction ){
+    //         console.log('up');
+    //         servo.to(0, 500);
+    //         servo2.to(160, 500);
+    //         direction = !direction
+    //     } else {
+    //         console.log('down');
+    //         servo.to(160, 500);
+    //         servo2.to(0, 500);
+    //         direction = !direction
+    //     }
+    // }
+    // sprinkler();
+    // servo.on('move:complete', sprinkler)
+
+    const leftWalk = new j5.Animation(servo);
+    const rightWalk = new j5.Animation(servo2);
+
+    leftWalk.enqueue({
+        cuePoints: [0, .33, 1],
+        keyFrames: [80, {value: 0, easing: "linear"}, {value: 160, easing: "linear"}],
+        duration: 3000,
+        loop: true
+    })
+    rightWalk.enqueue({
+        cuePoints: [0, .33, 1],
+        keyFrames: [80, {value: 160, easing: "linear"}, {value: 0, easing: "linear"}],
+        duration: 3000,
+        loop: true
+    })
     
-    function sprinkler(){
-        if( direction ){
-            console.log('up');
-            servo.to(30, 5000);
-            direction = !direction
-        } else {
-            console.log('down');
-            servo.to(120, 500);
-            direction = !direction
-        }
-        
-    }
-    sprinkler();
-    servo.on('move:complete', sprinkler)
+    
+
 })
